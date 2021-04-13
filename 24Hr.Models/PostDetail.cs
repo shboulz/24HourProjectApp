d1250b2c45f4015f6b6dba5c47c3a62e600320f6
Rochelle Deulley <rochelle.deulley@gmail.com>
2021-04-13 12:51:00 -0500

    models done for post

--- 24Hr.Models/PostDetail.cs
+++ 24Hr.Models/PostDetail.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,6 +8,15 @@ using System.Threading.Tasks;
 namespace _24Hr.Models
 {
     public class PostDetail
-    {
+    {   
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+
+        [Display(Name="Created")]
+        public DateTimeOffset CreatedUtc { get; set; }
+
+        [Display(Name="Modified")]
+        public DateTimeOffset? ModifiedUtc { get; set; }
     }
 }
