d68d4c40be4872fd8de6e263c7fddc03ec4a3ca2
Rochelle Deulley <rochelle.deulley@gmail.com>
2021-04-13 13:46:36 -0500

    done with service for post class

--- 24HR.Services/PostService.cs
+++ 24HR.Services/PostService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using _24HourProject.Models;
+using _24Hr.Models;
+using _24HR.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +11,65 @@ namespace _24HR.Services
 {
     public class PostService
     {
+        private readonly Guid _userId;
+
+        public PostService(Guid userId)
+        {
+            _userId = userId;   
+        }
+
+        public bool CreatePost(PostCreate model)
+        {
+            var entity = new Post()
+            {
+                AuthorId = _userId,
+                Title = model.Title,
+                Text = model.Text,
+            };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Post.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+            
+
+        public IEnumerable<PostListItem> GetPosts()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query = ctx
+                    .Posts
+                    .Where(e => e.Id == _userId)
+                    .Select(
+                    e =>
+                        new PostListItem
+                        {
+                            Id = e.Id,
+                            Title = e.Title,
+                            CreatedUtc = e.CreatedUtc
+                        }
+                        );
+                return query.ToArray();
+            }
+        }
+
+        public PostDetail GetPostById(int id)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                        .Posts
+                        .Single(e => e.PostId == id && e.AuthorId == _userId);
+                return
+                    new PostDetail
+                    {
+
+                    }
+            }
+        }
+        
     }
 }
