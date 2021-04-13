2795810765a0127019d352f4cf03929c7d73135c
shboulz <shboul89@gmail.com>
2021-04-13 12:01:41 -0400

    Initial Commit

---
+++ 24HR.Data/Post.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24HR.Data
+{
+    public class Post
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Text { get; set; }
+
+        public virtual List<Comment> Comments { get; set; } = new List<Comment>();
+
+        [Required]
+        public Guid AuthorId { get; set; }
+
+    }
+}
